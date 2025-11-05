493e0ab45e82b9726892256490d1a237778f3016
Senne Vandijck <12504770@student.pxl.be>
2025-11-05 14:23:01 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef een leeftijd in: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int age))
+            {
+                Console.WriteLine("Je bent " + age + " jaar oud.");
+            }
+            else
+            {
+                Console.WriteLine("Dat is geen geldig getal.");
+            }
+        }
+    }
+}
